029939d6fe77bbfa4b4e1adeecf449af1b1d364f
Igor Vlashchenko <iupgrader@mail.ru>
2021-10-24 07:55:40 +0300

    Прописывает необходимые атрибуты полям формы

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 import {createAdvertisements} from './mock/data-mock.js';
 import './utils.js';
 import {fillCardData} from './rendering.js';
-import {deactivateForm, activateForm} from './form.js';
+import {deactivateForm, activateForm, validateForm} from './form.js';
 
 document.querySelector('#map-canvas').appendChild(fillCardData(createAdvertisements()[0]));
 deactivateForm('map__filters');
 deactivateForm('ad-form');
 activateForm('map__filters');
 activateForm('ad-form');
+validateForm();
