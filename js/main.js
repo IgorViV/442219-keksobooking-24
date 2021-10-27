3be4561758790c327096387a99a981f6810ac816
IgorViV <iupgrader@mail.ru>
2021-10-27 22:08:16 +0300

    Переименовывает файл и функцию отрисовки карточки объявления, исправляет тернарные операторы

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 import {createAdvertisements} from './mock/data-mock.js';
 import './utils.js';
-import {fillCardData} from './rendering.js';
+import {renderCard} from './card.js';
 import {deactivateForm, activateForm, validateForm} from './form.js';
 
-document.querySelector('#map-canvas').appendChild(fillCardData(createAdvertisements()[0]));
+document.querySelector('#map-canvas').appendChild(renderCard(createAdvertisements()[0]));
 deactivateForm('map__filters');
 deactivateForm('ad-form');
 activateForm('map__filters');
