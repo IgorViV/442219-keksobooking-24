b0f4b0b0c81e5ec8f1e8d7e6fd53252acf0e29ba
Igor Vlashchenko <iupgrader@mail.ru>
2021-11-19 09:46:55 +0300

    Исправляет замечания по критериям Д5, Д10

--- js/filter.js
+++ js/filter.js
@@ -25,15 +25,15 @@ const priceFilter = {
  * @param {Function} cb Функция реализуемая при изменении фильтра
  */
 const applyFilter = (cb) => {
-  const onFilterListener = () => { // TODO Д4. Из названия обработчика события и функции-колбэка следует, что это обработчик.
+  const onFilterChange = () => { // TODO Д4. Из названия обработчика события и функции-колбэка следует, что это обработчик.
     resetMap();                    // onFilterListener - строго говоря, не подходит под критерий
     clearMarkerGroup();
     cb();
   };
 
-  formFilters.addEventListener('change', onFilterListener);
+  formFilters.addEventListener('change', onFilterChange);
 
-  formFilters.addEventListener('reset', onFilterListener);
+  formFilters.addEventListener('reset', onFilterChange);
 };
 
 /**
